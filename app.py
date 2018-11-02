a7179f45d72d6a7be1ae055243c5a0e44a6a7ad6
Bang <bangpham2701@gmail.com>
2018-11-02 11:28:22 -0500

    added endpoints and templates for login and registration

--- app.py
+++ app.py
@@ -9,11 +9,24 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
+@app.route('/register')
+def register():
+    return render_template("registration.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/landing')
+def landing():
+    return render_template("landing.html")
+
 @app.route('/time')
 def get_time():
     time_obj = time.Time()
     return jsonify(time_obj.get_time())
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    # app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=5000, debug=True)
 

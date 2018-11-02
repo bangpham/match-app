f44c581ce91f4de36a7cf022961638c57cc85866
Bang <bangpham2701@gmail.com>
2018-11-02 12:01:10 -0500

    added templates and endpoints

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import render_template
-from flask import Flask, jsonify
+from flask import Flask, redirect, url_for, request, jsonify
+
+
 from business import time
 
 app = Flask(__name__)
@@ -9,18 +11,44 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
+
+# @app.route('/success/<name>')
+# def success(name):
+#     return render_template("landing.html", name = name)
+
+@app.route('/success/<name>')
+def success(name):
+
+    #backend logic
+
+    return render_template("landing.html", name=name)
+
+
 @app.route('/register')
 def register():
-    return render_template("registration.html")
+    return render_template("index.html")
+
 
 @app.route('/login')
 def login():
     return render_template("login.html")
 
+
+@app.route('/click_login', methods=['POST', 'GET'])
+def click_login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name=user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name=user))
+
+
 @app.route('/time')
 def get_time():
     time_obj = time.Time()
     return jsonify(time_obj.get_time())
+
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
